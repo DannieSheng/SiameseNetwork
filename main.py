e1f8641da2dc201a1cfa9066c803d8a3b1a703ca
DannieSheng <hudanyun.sheng@gmail.com>
2019-09-11 23:40:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ import pickle
 # from lib.tools import EarlyStopping
 import lib.tools as tools
 import random
-from imblearn.over_sampling import SMOTE
 import pandas as pd
 
 import pdb
@@ -30,221 +29,30 @@ import pdb
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-
-plt.close('all')
-paras = trainlib.parameters
-
-# extract list of files containing specific classes from the summary file
-df_summary = pd.read_csv(os.path.join(paras['labelpath'], 'summary.csv'), index_col = False,  encoding = 'Latin-1')
-paras['filename'] = df_summary[df_summary.columns[0]].tolist()
-for n in paras['name_class']:
-    idx                                = df_summary[df_summary['class {}'.format(n)] >0].index
-    paras['filename{}'.format(n)] = [str(paras['filename'][i]) for i in idx]
-    
-list_file_all = {}
-len_all = []
-for classn in paras['name_class']:
-    list_file_all[str(classn)] = paras['filename'+ str(classn)]
-    len_all.append(len(list_file_all[str(classn)]))
-
-    # "good" wavelengths flag 
-flag             = sio.loadmat(os.path.join(paras['flagpath'], paras['flagname']), squeeze_me = True)
-goodWvlengthFlag = flag['flag']
-wavelength       = flag['wavelength']
+exp       = 0
+end_dim   = 30
+path_data = r'T:\AnalysisDroneData\Siamese\CLMB STND 2019 Flight Data\100081_2019_06_11_17_57_06\data\exp{}'.format(exp)
+path_exp  = path_data.replace('data', r'use_all_classes\{}'.format(end_dim))
 
     # patience for early stopping
 patience = 30
 
-    # fold for cross validation
-idx_fold  = 0
-count_all = 0
-while all(l>0 for l in len_all) & count_all<10:
-<<<<<<< HEAD
+for idx_fold in range(0,8):
+    path_data_fold = path_data + r'\fold{}'.format(idx_fold)
+    path_fold      = path_exp + r'\fold{}'.format(idx_fold)
+    paras          = pickle.load(open(os.path.join(path_fold, 'parameters.pkl'), 'rb'))
 
-	savepath_fold =  paras['savepath']  + '/fold' + str(idx_fold)
-	if not os.path.exists(savepath_fold):
-		os.makedirs(savepath_fold)     
-=======
-    savepath_data_fold = r'T:\Results\AnalysisDroneData\Siamese\CLMB STND 2019 Flight Data\100081_2019_06_11_17_57_06\usegt\use_all_classes\{}\saved_data\fold{}'.format(paras['end_dim'], idx_fold)
-    savepath_fold =  paras['savepath']  + '/fold' + str(idx_fold)
-    if not os.path.exists(savepath_fold):
-        os.makedirs(savepath_fold)     
->>>>>>> 8f2ad42e70c0083b23330ab611aa57df78a63ee5
-    
+        # "good" wavelengths flag 
+    flag             = sio.loadmat(os.path.join(paras['flagpath'], paras['flagname']), squeeze_me = True)
+    goodWvlengthFlag = flag['flag']
+    wavelength       = flag['wavelength']
+
+        # load pre-saved data
+    all_data       = pickle.load(open(os.path.join(path_data_fold, 'data.pkl'), 'rb'))
+    X_train y_train, X_valid, y_valid, X_test, y_test = all_data['X_train'], all_data['y_train'], all_data['X_valid'], all_data['y_valid'], all_data['X_test'], all_data['y_test']
+
+        # definition of early stopping
     early_stopping = trainlib.EarlyStopping(patience=patience, verbose=True)
-    
-<<<<<<< HEAD
-	count_all += 1
-	len_all = []
-
-    if os.path.exists(os.path.join(savepath_fold, 'data.pkl')):
-        paras    = pickle.load(open(os.path.join(savepath_fold, 'parameters.pkl'), 'rb'))
-        all_data = pickle.load(open(os.path.join(savepath_fold, 'data.pkl'), 'rb'))
-    else:
-    	paras['selected_file'] = []
-    	for idx, classn in enumerate(paras['name_class']):
-    		list_file = list_file_all[str(classn)]
-    		if idx >0:
-    			list_file = list(set(list_file)-set(paras['selected_file'])) # if file has been selected before, regard the file for this time 
-    		# selected_file = random.choice(list_file)
-    		selected_file = random.sample(list_file, 3)
-    		for f in selected_file:
-    		    list_file_all[str(classn)].remove(f)
-    		    paras['selected_file'].append(f)
-    	    
-    	for classn in paras['name_class']:
-    		len_all.append(len(list_file_all[str(classn)]))    
-
-    	for (idx, file) in enumerate(paras['selected_file']):
-    		for (idx_c, classn) in enumerate(paras['name_class']):
-    			if os.path.exists(os.path.join(paras['hyperpath'], 'raw_{}_{}.pkl'.format(file, classn))):
-    				spectra_class = pickle.load(open(os.path.join(paras['hyperpath'], 'raw_{}_{}.pkl'.format(file, classn)), 'rb'))
-    				spectra_class = spectra_class[:,np.where(goodWvlengthFlag == 1)[0]]
-    				label_class   = np.ones(np.shape(spectra_class)[0])*classn           
-    				if idx+idx_c == 0:
-    					spectra = spectra_class
-    					gt      = label_class
-    				else:
-    					spectra = np.concatenate((spectra, spectra_class), axis = 0)
-    					gt      = np.concatenate((gt, label_class), axis = 0)	     
-
-    	paras['num_class']  = len(np.unique(gt))
-    	paras['inputsize']  = np.shape(spectra)[1]
-
-        
-    	print('Data loading done!!')
-
-    	    # save parameters in a txt file and a pickle file 
-    	with open(os.path.join(savepath_fold, 'parameters.txt'), 'w') as f:
-    		for key, value in paras.items():
-    			f.write(key + ': ' + str(value) + '\n')
-    	f.close()
-    	pickle.dump(paras, open(os.path.join(savepath_fold, 'parameters.pkl'), 'wb'))
-
-    	    ## train-test split
-    	X_train_all, X_test, y_train_all, y_test, idx_train, idx_test = train_test_split(spectra, gt, range(0, len(gt)), test_size = 0.1, random_state = 0)
-    #	pdb.set_trace()
-    	    ## train-validation split
-    	X_train, X_valid, y_train, y_valid = train_test_split(X_train_all, y_train_all, test_size = 0.11, random_state = 0)
-
-    	# if there is one class with number smaller than 100000, upsampling
-    	# if all class numbers greater than 150000, reduce to 100000 (not finished)
-    	v, count = np.unique(y_train, return_counts = True)  
-    	if len(np.where(count > 100000)[0]) > 0:
-    		print('Downsampled those longer than 100000!\n')   
-    		for idx in np.where(count > 100000)[0]:
-    		#        idx = random.choice(np.where(count > 100000)[0])
-    			index = np.where(y_train == v[idx])[0]
-    			selected_id = np.array(random.sample(list(index), 100000))
-    			selected_X  = X_train[selected_id,:]
-    			selected_y  = y_train[selected_id]
-    			X_train = np.delete(X_train, index, axis = 0)
-    			y_train = np.delete(y_train, index)
-    			X_train = np.concatenate((X_train, selected_X), axis = 0)
-    			y_train = np.concatenate((y_train, selected_y), axis = 0)
-    		print('Downsampled those longer than 150000!\n')           
-    	if len(np.where(count < 100000)[0])>0:
-    		if len(np.where(count < 100000)[0])==2:  
-    			sm = SMOTE(random_state = 2, n_jobs = 16)
-    		else:
-    			sm = SMOTE(sampling_strategy = 'minority', random_state = 2, n_jobs = 16)
-    		print('Oversampled those longer than 100000!\n')               
-    		X_train, y_train = sm.fit_sample(X_train, y_train)
-    	else:
-    		print('All classes have greater than 100000 samples!')
-    	all_data = {'X_train': X_train,
-    				'y_train': y_train,
-    				'X_valid': X_valid,
-    				'y_valid': y_valid,
-    				'X_test': X_test,
-    				'y_test': y_test}
-        pickle.dump(all_data, open(os.path.join(savepath_fold, 'data.pkl'), 'wb'))
-=======
-    count_all += 1
-    len_all = []
-    if os.path.exists(os.path.join(savepath_data_fold, 'data.pkl')):
-        paras = pickle.load(open(os.path.join(savepath_data_fold, 'parameters.pkl'), 'rb'))
-        all_data = pickle.load(open(os.path.join(savepath_data_fold, 'data.pkl'), 'rb'))
-        X_train, y_train, X_valid, y_valid, X_test, y_test = all_data['X_train'], all_data['y_train'], all_data['X_valid'], all_data['y_valid'], all_data['X_test'], all_data['y_test']
-    else:
-        paras['selected_file'] = []
-        for idx, classn in enumerate(paras['name_class']):
-            list_file = list_file_all[str(classn)]
-            if idx >0:
-                list_file = list(set(list_file)-set(paras['selected_file'])) # if file has been selected before, regard the file for this time 
-            # selected_file = random.choice(list_file)
-            selected_file = random.sample(list_file, 3)
-            for f in selected_file:
-                list_file_all[str(classn)].remove(f)
-                paras['selected_file'].append(f)
-            
-        for classn in paras['name_class']:
-            len_all.append(len(list_file_all[str(classn)]))    
-
-        for (idx, file) in enumerate(paras['selected_file']):
-            for (idx_c, classn) in enumerate(paras['name_class']):
-                if os.path.exists(os.path.join(paras['hyperpath'], 'raw_{}_{}.pkl'.format(file, classn))):
-                    spectra_class = pickle.load(open(os.path.join(paras['hyperpath'], 'raw_{}_{}.pkl'.format(file, classn)), 'rb'))
-                    spectra_class = spectra_class[:,np.where(goodWvlengthFlag == 1)[0]]
-                    label_class   = np.ones(np.shape(spectra_class)[0])*classn           
-                    if idx+idx_c == 0:
-                        spectra = spectra_class
-                        gt      = label_class
-                    else:
-                        spectra = np.concatenate((spectra, spectra_class), axis = 0)
-                        gt      = np.concatenate((gt, label_class), axis = 0)	     
-
-        paras['num_class']  = len(np.unique(gt))
-        paras['inputsize']  = np.shape(spectra)[1]
-
-
-        print('Data loading done!!')
-
-            # save parameters in a txt file and a pickle file 
-        with open(os.path.join(savepath_fold, 'parameters.txt'), 'w') as f:
-            for key, value in paras.items():
-                f.write(key + ': ' + str(value) + '\n')
-        f.close()
-        pickle.dump(paras, open(os.path.join(savepath_fold, 'parameters.pkl'), 'wb'))
-
-            ## train-test split
-        X_train_all, X_test, y_train_all, y_test, idx_train, idx_test = train_test_split(spectra, gt, range(0, len(gt)), test_size = 0.1, random_state = 0)
-        #	pdb.set_trace()
-            ## train-validation split
-        X_train, X_valid, y_train, y_valid = train_test_split(X_train_all, y_train_all, test_size = 0.11, random_state = 0)
-
-        # if there is one class with number smaller than 100000, upsampling
-        # if all class numbers greater than 150000, reduce to 100000 (not finished)
-        v, count = np.unique(y_train, return_counts = True)  
-        if len(np.where(count > 100000)[0]) > 0:
-            print('Downsampled those longer than 100000!\n')   
-            for idx in np.where(count > 100000)[0]:
-            #        idx = random.choice(np.where(count > 100000)[0])
-                index = np.where(y_train == v[idx])[0]
-                selected_id = np.array(random.sample(list(index), 100000))
-                selected_X  = X_train[selected_id,:]
-                selected_y  = y_train[selected_id]
-                X_train = np.delete(X_train, index, axis = 0)
-                y_train = np.delete(y_train, index)
-                X_train = np.concatenate((X_train, selected_X), axis = 0)
-                y_train = np.concatenate((y_train, selected_y), axis = 0)
-            print('Downsampled those longer than 150000!\n')           
-        if len(np.where(count < 100000)[0])>0:
-            if len(np.where(count < 100000)[0])==2:  
-                sm = SMOTE(random_state = 2, n_jobs = 16)
-            else:
-                sm = SMOTE(sampling_strategy = 'minority', random_state = 2, n_jobs = 16)
-            print('Oversampled those longer than 100000!\n')               
-            X_train, y_train = sm.fit_sample(X_train, y_train)
-        else:
-            print('All classes have greater than 100000 samples!')
-        all_data = {'X_train': X_train,
-                    'y_train': y_train,
-                    'X_valid': X_valid,
-                    'y_valid': y_valid,
-                    'X_test': X_test,
-                    'y_test': y_test}
->>>>>>> 8f2ad42e70c0083b23330ab611aa57df78a63ee5
 
 ################### model definition#################
     model = trainlib.SiameseNetwork(paras['inputsize'], paras['end_dim'])	
